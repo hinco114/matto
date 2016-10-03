bf637c79ae2116f762cc759e2f1fc8b8607c3a80
SangHyoun <hainco@naver.com>
2016-10-04 02:29:28 +0900

    Using JSON Communication

--- matto_hardware/matto.cpp
+++ matto_hardware/matto.cpp
@@ -1,24 +1,23 @@
 /*********************************************************************
 Matto Project
-2016-09-23
+2016-10-03
 
 시리얼 통신 프로토콜
-아두이노가 읽는 방법 [명령어,값]
-아두이노가 보내는 방법 [상태,값1이름,값1,값2이름,값2]
-에러의 경우 [F,REASON,이유]
+JSON 형태로
+	1. "cmd" : "DOPEN"
+	문열기
+	2. "cmd" : "SENSING"
+	센싱값 전송
 
-적용된 명령어
-1. DOPEN = 문 열기
-2. DHT = 온도 습도 정보 보내기 [tem,온도,hum,습도]
-3. TISSUE = 휴지센서 값 받기
 
 핀번호 매칭 다시 해야함.
-DoorOpen 함수 구현 필요
-Timer 라이브러리 사용 여부(편할듯)
 
 *********************************************************************/
 
 // ----------------------------------선언부----------------------------------
+
+#include <Timer.h>	//Timer 라이브러리
+#include <ArduinoJson.h> //JSON 라이브러리
 
 //온습도센서
 #include "DHT.h"	//온습도센서 라이브러리 (DHT22)
@@ -35,17 +34,22 @@ Servo doorlock;
 //시간을 위한 변수
 unsigned long current_time=0;
 unsigned long prev_time=0;
+Timer t;
+int notPass=0;
 
 //조건들
 int interval_temp = 2000;	//온습도센서 카운트 딜레이
 int interval = 700;		//입구센서 카운트 딜레이
-float distance_enter = 30.0;
+//0A41SK 4~30cm 센서 센싱값 = 2076/(analogRead(A0)-11)
+//2A0A02 20~150cm 센서 센싱값 = 10650.08 * pow(Sensor_inner,-0.935) - 10;
+// 9462/(analogRead(A1)-16.92)
+float distance_enter = 10.0;
 
 //핀 셋팅
 int tissue_pin=4;		//휴지센서
 int tissue_led=5;		//휴지 LED
-char door_inner_pin=A0;		//입구 안쪽센서
-char door_outter_pin=A1;	//입구 바깥쪽센서
+char door_outter_pin=A0;		//입구 안쪽센서
+char door_inner_pin=A1;	//입구 바깥쪽센서
 int doorlock_pin=3;		//도어락(서보모터)
 
 
@@ -54,25 +58,29 @@ float door_sens[2];
 int tissue_sens;
 int sector_sens[2];
 int number_person=0;
+int save_person=0;
 
 //통신에 필요한 변수
-String in_data="";
+char in_data[300] = "";
 bool needCommu=false;
+bool endCommu=false;
+
 // ----------------------------------선언부끝----------------------------------
 
 // ----------------------------------메인부----------------------------------
 void setup(){
+	pinMode(tissue_led,OUTPUT);	
 	Serial.begin(9600);
-	dht.begin();	//온습도센서 구현. 센싱에 250ms 소요
+	dht.begin();	//온습도센서 로딩
 	doorlock.attach(doorlock_pin);	//도어락 연결
-	pinMode(tissue_led,OUTPUT);
+	t.every(60*1000,sensing_tissue);	//1분마다 휴지체크
 }
 
 void loop(){
+	t.update();
 	current_time = millis();
 	sensing_door(door_inner_pin,door_outter_pin);	//입구센서 센싱
 	doorCount();	//센싱값으로 인원처리
-	sensing_tissue();	//휴지센서 처리
 	if(Serial.available()){
 		fromSerial();	//시리얼통신으로 받을게 있으면 커뮤니케이션 함수 실행
 	}
@@ -82,77 +90,64 @@ void loop(){
 
 
 // ----------------------------------통신부----------------------------------
-// [상태,값1이름,값1,값2이름,값2]형태로 시리얼 전송
-// 값1이름이나 값2이름이이 "0"인 경우 
-// 상태가 0으로 동작이 실패한 경우, 값1이름으로 REASON을 받고 [status,REASON,내용] 전송
-void toSerial(int status,String dataName1,int data1,String dataName2,int data2){
-	char s = status==1?'S':'F';
-	Serial.print("[");
-	Serial.print(s);
-	if(dataName1=="0"){		//dataName1 가 "0"이면 끝냄
-		Serial.println("]");
-		return;
-	}
-	Serial.print(",");
-	Serial.print(dataName1);
-	Serial.print(",");
-	if (dataName1=="REASON"){	//값1이름이 REASON인 경우
-		Serial.print(dataName2);
-		Serial.println("]");
-		return;
-	}
-	Serial.print(data1);
-	if(dataName2 == "0"){	//dataName2 가 "0"이면 끝냄
-		Serial.println("]");
-		return;
-	}
-	Serial.print(",");
-	Serial.print(dataName2);
-	Serial.print(",");
-	Serial.print(data2);
-	Serial.println("]");
-}
-
-void fromSerial(){	//통신관련 함수
+void fromSerial(){	//수신 코드
+	static int i=0;
 	char data = Serial.read();	//일단 읽음
 	if(needCommu){			//읽는 중이었다면
-		if(data == ']'){	//마지막을 알리는 경우 설정갑 초기화와 함께 파싱 진행
-			parseData(in_data);
+		if(data == '}'){	//마지막을 알리는 경우 설정갑 초기화와 함께 파싱 진행
+			in_data[i++]=data;
+			in_data[i]='\0';
+			i=0;
 			needCommu = false;
-			in_data="";
+			endCommu = true;
 		}
 		else{
-			in_data += data;	//내용의 중간인 경우 in_data에 계속 추가
+			in_data[i++] = data;	//내용의 중간인 경우 in_data에 계속 추가
 		}
 	}
 	else{
-		if(data == '['){
+		if(data == '{'){
 			needCommu = true;	//내용의 시작을 알리면 읽기 모드 돌입
+			in_data[i++]=data;
 		}
+	}
+	if(endCommu){
+		//시리얼 값 다 받으면 JSON파싱
+		StaticJsonBuffer<200> jsonBufferIn;
+		JsonObject& recv = jsonBufferIn.parseObject(in_data);
+		//JSON분석을 통한 실행
+		parseData(recv);
+		endCommu=false;
 	}
 }
 
-void parseData(String needParse){
-	String cmd = needParse.substring(0,needParse.indexOf(','));
-	String data = needParse.substring(needParse.indexOf(',')+1);
-	if(cmd == "DOPEN"){
-		// doorOpen();
-		doorlock.write(data.toInt());
-		
-		toSerial(1,"0",0,"0",0);
+void parseData(JsonObject& parse){
+	StaticJsonBuffer<200> jsonBufferOut;	//버퍼설정
+	JsonObject& send = jsonBufferOut.createObject();
+	char* cmd = parse["cmd"];	//명령어 파싱
+	//명령어가 DOPEN이면
+	if(strcmp(cmd,"DOPEN")==0){
+		doorOpen();
+		send["status"] = "S";
 	}
-	else if(cmd == "DHT"){	//DHT명령인 경우 온습도 센싱 실행후 반환
+	//명령어가 SENSING이면
+	else if(strcmp(cmd,"SENSING")==0){
+		//JSON오브젝트에 각 값을 추가
+		send["status"] = "S";
 		sensing_dht();
-		if (isnan(humidity) || isnan(temperature)) {
-			toSerial(0,"REASON",0,"Failed to read from DHT sensor",0); 
+		if (isnan(humidity) || isnan(temperature)){}
+		else {
+			send["tem"] = temperature;
+			send["hum"] = humidity;
 		}
-		else { 
-			toSerial(1,"TEM",temperature,"HUM",humidity);//시리얼 통신에 보냄
-		}
+		send["tissue"] = tissue_sens;
+		send["count"] = number_person;	
 	}
-	else if(cmd == "TISSUE"){	//TISSUE명령어인 경우 tissue_sens값 반환
-		toSerial(1,"Tissue Data",tissue_sens,"0",0);
+	else{
+		send["status"] = "F";
 	}
+	//값 시리얼로 전송
+	send.printTo(Serial); Serial.println("");
 }
 // ----------------------------------통신부끝----------------------------------
 
@@ -161,9 +156,18 @@ void parseData(String needParse){
 
 // Delay 없이 타이머를 이용하고, 일정시간 지나면 문이 잠기도록 해야함
 void doorOpen(){
+	Serial.println("Door OPEND!");
 	doorlock.write(21);
-	delay(1500);
-	doorlock.write(95);
+	save_person = number_person;
+	notPass = t.every(1000,doorClose);
+}
+void doorClose(){
+	Serial.println("Not Closed");
+	if((notPass!=0) && (number_person>save_person)){
+		doorlock.write(95);
+		t.stop(notPass);
+		notPass=0;
+	}
 }
 
 void sensing_tissue(){
@@ -181,9 +185,9 @@ void sensing_dht(){
 void sensing_door(char sensor_inner, char sensor_outter){	
 	int Sensor_inner = analogRead(sensor_inner);
 	int Sensor_outter = analogRead(sensor_outter);
-	float cm_inner = 10650.08 * pow(Sensor_inner,-0.935) - 10;	//그래프를 수식화한 식
-	float cm_outter = 10650.08 * pow(Sensor_outter,-0.935) - 10;
-	
+	float cm_inner = 2076/(Sensor_inner-11);	//그래프를 수식화한 식
+	float cm_outter = 2076/(Sensor_outter-11);
+	//Serial.print(cm_outter); Serial.print("  "); Serial.println(cm_inner); 
 	//안쪽센서 센서범위 내 인 경우
 	if(cm_inner<distance_enter)
 		door_sens[0] = 1;
@@ -191,7 +195,7 @@ void sensing_door(char sensor_inner, char sensor_outter){
 		door_sens[0] = 0;
 	
 	//바깥쪽센서 센서범위 내 인 경우
-	if((cm_outter<distance_enter))
+	if(cm_outter<distance_enter)
 		door_sens[1] = 1;
 	else
 		door_sens[1] = 0;

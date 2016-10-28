0d2b3354a4025f0483f3f1db95ed9e0e6e06b29a
최한규 <hangyu0912@gmail.com>
2016-10-29 03:10:25 +0900

    modified the length of the imgSrc and description.

--- matto_server/models/product.js
+++ matto_server/models/product.js
@@ -6,8 +6,8 @@ module.exports = function(sequelize, DataTypes) {
 	    productIdx : {type : DataTypes.INTEGER, primaryKey : true,  autoIncrement: true, allowNull : false},
 	    name : {type : DataTypes.STRING(15), allowNull : false},
 	    price : {type : DataTypes.INTEGER},
-	    imgSrc : {type : DataTypes.STRING(50)}, 
-	    description : {type : DataTypes.STRING(30)}
+	    imgSrc : {type : DataTypes.STRING(200)}, 
+	    description : {type : DataTypes.STRING(50)}
 	  }, {
 		    classMethods: {},
 		    tableName: 'products',
